a95eac864412ab8e24a6f25513420fe711b46133
Akram-dino <akramssemwanga49@gmail.com>
2026-05-06 12:24:25 +0300

    auto create users

--- buhara_teanet/build.sh
+++ buhara_teanet/build.sh
@@ -4,3 +4,29 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+python manage.py shell -c "
+from accounts.models import User;
+
+if not User.objects.filter(username='admin1').exists():
+    User.objects.create_superuser(
+        username='admin1',
+        email='admin@test.com',
+        password='Admin12345'
+    );
+
+if not User.objects.filter(username='worker1').exists():
+    User.objects.create_user(
+        username='worker1',
+        password='Worker12345',
+        role='worker'
+    );
+
+if not User.objects.filter(username='reviewer1').exists():
+    User.objects.create_user(
+        username='reviewer1',
+        password='Reviewer12345',
+        role='reviewer'
+    );
+"
